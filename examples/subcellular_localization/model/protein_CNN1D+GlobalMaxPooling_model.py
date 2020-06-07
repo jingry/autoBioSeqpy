630353d7e38ce834725a60099bb211f62ac1ebf5
Runyu Jing <jingryedu@gmail.com>
2020-06-07 15:11:11 +0800

    few modification for the notebook and related files

--- examples/subcellular_localization/model/protein_CNN1D+GlobalMaxPooling_model.py
+++ examples/subcellular_localization/model/protein_CNN1D+GlobalMaxPooling_model.py
@@ -4,44 +4,31 @@ from keras.layers import Embedding
 from keras.layers import Conv1D, GlobalMaxPooling1D
 from keras.utils import np_utils
 from keras import optimizers
-from keras_self_attention import SeqSelfAttention
-from keras.layers import LSTM, Bidirectional
-from keras.layers import Conv1D, MaxPooling1D
-from keras.layers import Flatten
+
+
 
 
 max_features = 26
 embedding_size = 256
 filters = 250
 kernel_size = 5
-hidden_dims = 650
-lstm_output_size = 64
-pool_size = 2
-input_length = 400
+hidden_dims = 2000
 
 print('Building model...')
 model = Sequential()
-model.add(Embedding(max_features, embedding_size, input_length=input_length))
+model.add(Embedding(max_features, embedding_size))
 model.add(Dropout(0.25))
 model.add(Conv1D(filters,kernel_size,padding = 'valid',activation = 'relu',strides = 1))
-#model.add(GlobalMaxPooling1D())
-#model.add(Dense(hidden_dims))
-#model.add(Dropout(0.5))
-#model.add(Activation('relu'))
-
-model.add(MaxPooling1D(pool_size = pool_size))
-
-model.add(Bidirectional(LSTM(units=lstm_output_size, return_sequences=True)))
-model.add(SeqSelfAttention(attention_activation='sigmoid'))
-
-model.add(Flatten())
-
+model.add(GlobalMaxPooling1D())
+model.add(Dense(hidden_dims))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
 model.compile(loss = 'categorical_crossentropy',optimizer = optimizers.Adam(),metrics = ['acc'])
 #model.summary()
-custom_objects=SeqSelfAttention.get_custom_objects()
+
 #model = Sequential()
 #model.add(Embedding(max_features, embedding_size, input_length = 2000))
 #model.add(Dropout(0.25))
---
+++ examples/subcellular_localization/model/protein_CNN1D+MaxPooling_bi_att.py
@@ -0,0 +1,41 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import Conv1D, GlobalMaxPooling1D
+from keras.utils import np_utils
+from keras import optimizers
+from keras_self_attention import SeqSelfAttention
+from keras.layers import LSTM, Bidirectional
+from keras.layers import Conv1D, MaxPooling1D
+from keras.layers import Flatten
+
+
+max_features = 26
+embedding_size = 256
+filters = 250
+kernel_size = 5
+hidden_dims = 650
+lstm_output_size = 64
+pool_size = 2
+input_length = 2000
+
+print('Building model...')
+model = Sequential()
+model.add(Embedding(max_features, embedding_size, input_length=input_length))
+model.add(Dropout(0.25))
+model.add(Conv1D(filters,kernel_size,padding = 'valid',activation = 'relu',strides = 1))
+
+model.add(MaxPooling1D(pool_size = pool_size))
+
+model.add(Bidirectional(LSTM(units=lstm_output_size, return_sequences=True)))
+model.add(SeqSelfAttention(attention_activation='sigmoid'))
+
+model.add(Flatten())
+
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.compile(loss = 'categorical_crossentropy',optimizer = optimizers.Adam(),metrics = ['acc'])
+custom_objects=SeqSelfAttention.get_custom_objects()
+
+#model.summary()
